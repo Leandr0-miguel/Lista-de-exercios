b45b3b44a32bb27fea31fc66b5a3163a15f31bff
Leandr0-miguel <leandro170108@edu.unipar.br>
2026-05-27 22:24:53 -0300

    Exercício 1 completo
    Feito a inserção do arquivo CSS

--- Lista de exerrcicios/JS/script.js
+++ Lista de exerrcicios/JS/script.js
@@ -1,22 +1,51 @@
 // Variaveis exercicio 1
-let inputX = document.getElementById("inputX")
-let inputY = document.getElementById("inputY")
-let inputZ = document.getElementById("inputZ")
+let inputX = document.getElementById("inputX");
+let inputY = document.getElementById("inputY");
+let inputZ = document.getElementById("inputZ");
 
-let btnTriangulo = document.getElementById("btnTriangulo")
+let btnTriangulo = document.getElementById("btnTriangulo");
 
-let resultadoTriangulo = document.getElementById("resultadoTriangulo")
+let resultadoTriangulo = document.getElementById("resultadoTriangulo");
 
 
 function verificarTriangulo(){
-    x = inputX.value;
-    y = inputY.value;
-    z = inputZ.value;
+    let x = Number(inputX.value);
+    let y = Number(inputY.value);
+    let z = Number(inputZ.value);
 
-    let valores_triangulos = [x, y, z]
-    
+    let valores_triangulo = [x, y, z]
     let soma_valores = 0
     
+    for(let i=0; i<valores_triangulo.length; i++){
+        for(let j=0; j<valores_triangulo.length; j++){
+            if (i != j){
+                soma_valores += valores_triangulo[j];
+            }
+        }
+
+        if (valores_triangulo[i] >= soma_valores){
+            resultadoTriangulo.value = "Valores informados não formam um triangulo!"
+            return;
+        }
+        soma_valores = 0;
+    }
+    definirTriangulo(valores_triangulo)
+}
+
+function definirTriangulo(lados){
+    let tipo_triangulo = "";
+
+    if(lados[0] === lados[1] && lados[0] === lados[2]){
+        tipo_triangulo = "Equilatero";
+    } else if(lados[0] === lados[1] || lados[0] === lados[1] || lados[1] === lados[2]){
+        tipo_triangulo = "Iseoceles";
+    } else{
+        tipo_triangulo = "Escaleno";
+    }
+
+
+    resultadoTriangulo.value = tipo_triangulo;
+    return;
 }
 
 btnTriangulo.addEventListener("click", verificarTriangulo);
