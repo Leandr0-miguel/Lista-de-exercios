e081f02a87ad3c83a5ba105e588f0188e22df510
Leandr0-miguel <leandro170108@edu.unipar.br>
2026-06-10 10:25:42 -0300

    Add files via upload

--- Lista de exerrcicios/JS/script.js
+++ Lista de exerrcicios/JS/script.js
@@ -8,6 +8,9 @@ let btnTriangulo = document.getElementById("btnTriangulo");
 let resultadoTriangulo = document.getElementById("resultadoTriangulo");
 
 
+
+
+// Exercicio 1
 function verificarTriangulo(){
     let x = Number(inputX.value);
     let y = Number(inputY.value);
@@ -22,7 +25,7 @@ function verificarTriangulo(){
                 soma_valores += valores_triangulo[j];
             }
         }
-
+        
         if (valores_triangulo[i] >= soma_valores){
             resultadoTriangulo.value = "Valores informados não formam um triangulo!"
             return;
@@ -42,10 +45,48 @@ function definirTriangulo(lados){
     } else{
         tipo_triangulo = "Escaleno";
     }
-
-
+    
+    
     resultadoTriangulo.value = tipo_triangulo;
     return;
 }
 
 btnTriangulo.addEventListener("click", verificarTriangulo);
+
+
+//Variaveis exercicio 2
+let inputPeso = document.getElementById("inputPeso");
+let inputAltura = document.getElementById("inputAltura");
+
+let btnIMC = document.getElementById("btnIMC")
+
+let resultadoIMC = document.getElementById("resultadoIMC");
+let statusIMC = document.getElementById("statusIMC");
+
+
+//Exercicio 2
+function calcIMC(){
+    let altura = Number(inputAltura.value)
+    let peso = Number(inputPeso.value)
+    let IMC = peso / (altura * altura)
+
+    
+    if(IMC < 18.5){
+        statusIMC.textContent = "Abaixo do peso";
+    } else if(IMC  > 18.5 && IMC <= 24.9){
+        statusIMC.textContent = "Peso normal";
+    } else if(IMC >= 25 && IMC <= 29.9){
+        statusIMC.textContent = "Sobrepeso";
+    } else if(IMC >= 30 && IMC <= 34.9){
+        statusIMC.textContent = "Obesidade grau 1";
+    } else if(IMC >= 35 && IMC <= 39.9){
+        statusIMC.textContent = "Obesidade grau 2";
+    } else{
+        statusIMC.textContent = "Obesidade grau 3";
+    }
+
+
+    resultadoIMC.textContent = `Seu IMC: ${IMC.toFixed(2)}`
+}
+
+btnIMC.addEventListener("click", calcIMC)
